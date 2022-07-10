f60d391f10d971aaf119c19d56148f88a45c7a29
0ho0ho <y2k6273@naver.com>
2022-07-10 21:09:17 +0900

    Express Basics

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+const express = require('express');
+const app = express();
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  console.log(url);
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
-  } else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  } else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
-  } else if (url ==='/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
-  } else if (url ==='/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  } else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
-
+app.get('/', (req, res) => {
+  console.log('user hit the resource');
+  res.status(200).send('Home Page');
 })
 
-server.listen(5000)
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page');
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>resouce not found<h1>');
+})
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000')
+})
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
