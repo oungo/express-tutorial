8c99118a0ceec6fc6391727bd58528a6dd5da6f3
0ho0ho <y2k6273@naver.com>
2022-07-11 22:44:12 +0900

    JSON Basics

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const { products } = require('./data');
 
-// setup static and middleware
-app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// adding to static assets
-// SSR
-// })
-
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+app.get('/', (req, res) => {
+  res.json(products)
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....');
+  console.log('Server is listening on port 5000....');
 })
