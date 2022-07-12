6f8eb1b7c10e84bc8ed967bc08d04549cf5ddc02
0ho0ho <y2k6273@naver.com>
2022-07-12 22:34:29 +0900

    Params - Extra Info

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.get('/api/products/:productID', (req, res) =>{
   return res.json(singleProduct);
 });
 
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('hello world')
+})
+
 
 
 app.listen(5000, () => {
