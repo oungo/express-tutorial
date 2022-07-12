a0e48c750b64d7eefe8d43463993bb2bc73ba325
0ho0ho <y2k6273@naver.com>
2022-07-12 22:47:23 +0900

    Query String

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Home Page</h1><a href="/api/products">productc</a>')
 });
 
-app.get('/api/products', (req, res) =>{
+app.get('/api/products', (req, res) => {
   const newProducts = products.map(product => {
     const { id, name, image } = product;
     return { id, name, image }
@@ -29,7 +29,24 @@ app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/api/v1/query', (req, res) => {
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
 
+  if (search) {
+    sortedProducts = sortedProducts.filter(product => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    // res.status(200).send('no products matched your search');
+    return res.status(200).json({ success: true, data: [] })
+  }
+  res.status(200).json(sortedProducts)
+})
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....');
