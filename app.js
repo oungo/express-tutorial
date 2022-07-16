d03a9c502c9a089c43da14cb6b5e7233a548dea1
oungo <y2k6273@naver.com>
2022-07-16 14:47:39 +0900

    Methods - DELETE

--- app.js
+++ app.js
@@ -47,6 +47,17 @@ app.put('/api/people/:id', (req, res) => {
 
 })
 
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res.status(400).json({success: false, msg: `no person with id ${req.params.id}`})
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+  return res.status(200).json({success: true, data: newPeople})
+})
+
 app.post('/login', (req, res) => {
   const { name } = req.body;
   if (name) {
