8e3ffd0f9e6ddc212f23ae18d3c9ed1903e46cc9
oungo <y2k6273@naver.com>
2022-07-16 14:30:58 +0900

    Install Postman

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.post('/api/people', (req, res) => {
   res.status(201).send({success: true, person: name})
 })
 
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({success: false, msg: 'please provide name value'})
+  }
+  return res.status(201).json({ success: true, person: [...people, name]})
+})
+
 app.post('/login', (req, res) => {
   const { name } = req.body;
   if (name) {
